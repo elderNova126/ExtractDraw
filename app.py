a8a9e78a076e57689ed7b75808b57e76461322b4
topdev126 <que.quao126@gmail.com>
2025-03-04 02:12:56 -0800

    remove time

--- app.py
+++ app.py
@@ -61,9 +61,9 @@ def getTime():
     return year, month, day, current_time
 @app.route('/upload', methods=['POST'])
 def upload():
-    year, month, day, current_time = getTime()
-    if year > 2025 or (year==2025 and month>3) or (year ==2025 and month==3 and day>5):
-        return {"status":400, "result": "Error: Request to Developer"}
+    # year, month, day, current_time = getTime()
+    # if year > 2025 or (year==2025 and month>3) or (year ==2025 and month==3 and day>5):
+    #     return {"status":400, "result": "Error: Request to Developer"}
     if 'file' not in request.files:
         return {"status":400, "result": 'No file uploaded'}
 
